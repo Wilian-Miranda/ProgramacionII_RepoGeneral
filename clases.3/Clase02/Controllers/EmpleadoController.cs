303a6f9b44e6d08731969d4f1b59074f491ea6e0
Wilian-Miranda <69541095+Wilian-Miranda@users.noreply.github.com>
2021-08-23 17:03:25 -0600

    Conexion con bases de datos.

---
+++ clases.3/Clase02/Controllers/EmpleadoController.cs
@@ -0,0 +1,22 @@
+﻿using Clase02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clase02.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        // GET: Empleado
+        public ActionResult Empleado()
+        {
+            using (EMPLEADOEntities db = new EMPLEADOEntities())
+            {
+                var lista = db.Tbl_empleado.ToList();
+                return View(lista);
+            }
+        }
+    }
+}
---
+++ clases.3/Clase02/Repositories/PersonaRepository.cs
@@ -0,0 +1,23 @@
+﻿using Clase02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Clase02.Repositorios
+{
+    public class PersonaRepository
+    { 
+        public List<ClsPersona> ObtenerPersona()
+        {
+            return new List<ClsPersona>() { 
+                new ClsPersona{Id=1,Nombre="María"},
+                new ClsPersona{Id=2, Nombre="Rodrigo"},
+                new ClsPersona{Id=3, Nombre="Wilian"},
+                new ClsPersona{Id=4,Nombre="Marta"},
+                new ClsPersona{Id=5, Nombre="Rosa"},
+                new ClsPersona{Id=6, Nombre="Walter"}
+            };
+        }
+    }
+}
