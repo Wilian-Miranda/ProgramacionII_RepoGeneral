e51931d12b120b1f0bc0bf9994bbaf9509ed6320
Wilian-Miranda <69541095+Wilian-Miranda@users.noreply.github.com>
2021-08-26 15:33:45 -0600

    Clase 3.2 y 4
    Crud en Asp.net con razor pages. Crud manual.

---
+++ clases.3.2/Clase02/Controllers/PruebaController.cs
@@ -0,0 +1,24 @@
+﻿using Clase02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clase02.Controllers
+{
+    public class PruebaController : Controller
+    {
+        // GET: Prueba
+        public ActionResult PruebaVista()
+        {
+            //invocación a clase
+            var prueba = new ClsPrueba
+            {
+                Id = 1,
+                Nombre = "Prueba"
+            };
+            return View(prueba);
+        }
+    }
+}
