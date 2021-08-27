2c9a41b8de6705639bce424e077b934b6b8ee425
Wilian-Miranda <69541095+Wilian-Miranda@users.noreply.github.com>
2021-08-27 12:08:56 -0600

    Formulario index para capturar los datos

---
+++ Parte_practica_del_Laboratorio1/GestionNotas/GestionNotas/Controllers/NotasController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionNotas.Controllers
+{
+    public class NotasController : Controller
+    {
+        // GET: Notas
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult Save()
+        {
+
+        }
+    }
+}
